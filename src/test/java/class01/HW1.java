4848e237bfc04605e1c035eac8e05b6011b9da5c
Ramazan774 <ramazan.ramazanov@glion.ch>
2022-05-14 19:43:59 -0700

    first commit

---
+++ src/test/java/class01/HW1.java
@@ -0,0 +1,13 @@
+package class01;
+
+import sun.applet.Main;
+
+public class HW1 {
+
+    public static void main(String[] args) {
+        for (int i= 1;i<=100;i++){
+            System.out.println(i);
+        }
+
+    }
+}
