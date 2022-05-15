3eb516992c92b45cacc85e63d099f24deed870a6
Ramazan774 <ramazan.ramazanov@glion.ch>
2022-05-14 20:07:51 -0700

    final commit

--- src/test/java/class01/HW1.java
+++ src/test/java/class01/HW1.java
@@ -5,7 +5,7 @@ import sun.applet.Main;
 public class HW1 {
 
     public static void main(String[] args) {
-        for (int i= 1;i<=100;i++){
+        for (int i= 1;i<=50;i++){
             System.out.println(i);
         }
 
